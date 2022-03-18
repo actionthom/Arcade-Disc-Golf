272fe632cd3eee5cfa6656c4740b37fef6b372fe
actionthom <actionthom@gmail.com>
2022-03-18 10:21:02 +0000

    attempt to add angled throws

--- Assets/Disc/Disc.cs
+++ Assets/Disc/Disc.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 public class Disc : MonoBehaviour
@@ -7,9 +8,25 @@ public class Disc : MonoBehaviour
 	[SerializeField] private float _maxFlightTime = 3f;
 	[SerializeField] private AnimationCurve _flightDecelerationCurve;
 
+	private Vector3 _targetPoint = Vector3.zero;
+
 	private void Awake()
 	{
-		ThrowControls.ThrowEvent += OnThrowEvent;
+		ThrowInput.ThrowEvent += OnThrowEvent;
+		ThrowInput.TargetSetEvent += OnTargetSet;
+		ThrowInput.ReleaseAngleSetEvent += OnReleaseAngleSet;
+	}
+
+	private void OnReleaseAngleSet(Vector3 mousePos)
+	{
+		mousePos.z = transform.position.z;
+		transform.up = (mousePos - transform.position).normalized;
+	}
+
+	private void OnTargetSet(Vector3 targetPosition)
+	{
+		targetPosition.z = transform.position.z;
+		_targetPoint = targetPosition;
 	}
 
 	private void OnThrowEvent(float power)
@@ -22,14 +39,18 @@ public class Disc : MonoBehaviour
 		float speed = _maxSpeed * power;
 		float flightTime = _maxFlightTime * power;
 		float currentTime = flightTime;
+		Vector3 startDirection = transform.up;
+		Vector3 reflectedPosition = Vector3.Reflect(startDirection, (transform.position - _targetPoint).normalized);
+		Vector3 endDirection = (reflectedPosition - transform.position).normalized;
 
 		while (currentTime > 0)
 		{
+			float timeDelta = 1 - (currentTime / flightTime);
+			transform.up = Vector3.Lerp(startDirection, endDirection, timeDelta);
 			transform.localPosition += (transform.up * speed);
 			currentTime -= Time.deltaTime;
-			float delta = _flightDecelerationCurve.Evaluate(1 - (currentTime / flightTime));
-			speed *= delta;
-			Debug.Log("speed = " + speed);
+	
+			speed *= _flightDecelerationCurve.Evaluate(timeDelta);
 			if (speed < MIN_SPEED)
 			{
 				yield break;
@@ -41,6 +62,19 @@ public class Disc : MonoBehaviour
 
 	private void OnDestroy()
 	{
-		ThrowControls.ThrowEvent -= OnThrowEvent;
+		ThrowInput.ThrowEvent -= OnThrowEvent;
+		ThrowInput.TargetSetEvent -= OnTargetSet;
+		ThrowInput.ReleaseAngleSetEvent -= OnReleaseAngleSet;
+	}
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.white;
+		Gizmos.DrawSphere(_targetPoint, 0.1f);
+		Gizmos.color = Color.green;
+		Gizmos.DrawLine(transform.position, _targetPoint);
+		Gizmos.color = Color.blue;
+		Vector3 lineTarget = transform.position + (transform.up * 50);
+		Gizmos.DrawLine(transform.position, lineTarget);
 	}
 }
--- Assets/Input/ThrowInput.cs
+++ Assets/Input/ThrowInput.cs
@@ -2,12 +2,15 @@ using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class ThrowControls : MonoBehaviour
+public class ThrowInput : MonoBehaviour
 {
 	private Mouse _currentMouse;
 	private Vector2 _startPos;
 	[SerializeField] private float _maxDistance = 4000f;
+
 	public static event Action<float> ThrowEvent;
+	public static event Action<Vector3> TargetSetEvent;
+	public static event Action<Vector3> ReleaseAngleSetEvent;
 
 	private void Update()
 	{
@@ -27,7 +30,11 @@ public class ThrowControls : MonoBehaviour
 		{
 			Debug.Log("Left Button Pressed");
 			_startPos = _currentMouse.position.ReadValue();
-
+			TargetSetEvent?.Invoke(GetMouseWorldPos(_startPos));
+		}
+		else if(_currentMouse.leftButton.isPressed)
+		{
+			ReleaseAngleSetEvent?.Invoke(GetMouseWorldPos(_currentMouse.position.ReadValue()));
 		}
 		else if (_currentMouse.leftButton.wasReleasedThisFrame && _startPos != Vector2.zero)
 		{
@@ -39,5 +46,11 @@ public class ThrowControls : MonoBehaviour
 			}
 			Debug.Log("Released with distance of " + (distance / _maxDistance));
 		}
+	}
+
+	private Vector3 GetMouseWorldPos(Vector2 mousePos)
+	{
+		Camera cam = Camera.main;
+		return cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, -10)); //todo - set this from the Plane Distance in the Canvas
 	}
 }
